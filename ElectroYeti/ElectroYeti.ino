ef555a1d95307bcb1fc412ae4cc3a71ed9a83abe
lal-sah <sah.lalbabu@gmail.com>
2019-11-18 07:44:53 -0600

    Integrated SCT-013-000 program

--- ElectroYeti/ElectroYeti.ino
+++ ElectroYeti/ElectroYeti.ino
@@ -7,6 +7,12 @@
 #include <dht11.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "EmonLib.h"             // Include Emon Library
+
+#define VOLT_CAL 148.7
+#define CURRENT_CAL 62.6
+
+EnergyMonitor emon1;             // Create an instance
 
 #define DHT11PIN A1
 
@@ -15,13 +21,16 @@ LiquidCrystal_I2C lcd(0x3F, 20, 4); // set the LCD address to 0x27 for a 16 char
 dht11 DHT11;
 float temperature;
 float humidity;
-float amps = 0.0;
-float watts = 0.0;
+float currentDraw = 0.0;
+float powerUsage = 0.0;
 
 
 void setup()
 {
   Serial.begin(9600);
+
+  emon1.voltage(1, VOLT_CAL, 1.7);  // Voltage: input pin, calibration, phase_shift
+  emon1.current(0, CURRENT_CAL);       // Current: input pin, calibration.
 
   lcd.init();
 }
@@ -32,7 +41,7 @@ void loop()
   measureTH();
   measureCurrentPower();
   displayOnLCD();
-  //  sendIFTTTSMS();
+  sendIFTTTSMS();
   delay(5000);
 
 }
@@ -51,22 +60,39 @@ void measureTH() {
 }
 
 void measureCurrentPower() {
+  emon1.calcVI(20, 2000);        // Calculate all. No.of half wavelengths (crossings), time-out
 
+  currentDraw = emon1.Irms;             //extract Irms into Variable
+  float supplyVoltage = emon1.Vrms;                    //extract Vrms into Variable
+
+  //make it fixed 120V for demo
+  supplyVoltage = 120;
+
+  Serial.print("Voltage: ");
+  Serial.println(supplyVoltage);
+
+  Serial.print("Current: ");
+  Serial.println(currentDraw);
+
+  powerUsage = currentDraw * supplyVoltage;
+  Serial.print("Watts: ");
+  Serial.println(powerUsage);
+  Serial.println("");
 }
 
 void displayOnLCD() {
   lcd.init(); // initialize the lcd
   lcd.init();
-  
+
   // Print current & power usage to the LCD.
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Amps: ");
-  lcd.print(amps);
+  lcd.print(currentDraw);
 
   lcd.setCursor(0, 1);
   lcd.print("Watts: ");
-  lcd.print(watts);
+  lcd.print(powerUsage);
 
   // Print temperature & humidity to the LCD.
   lcd.setCursor(0, 2);
@@ -76,7 +102,7 @@ void displayOnLCD() {
   lcd.print("H: ");
   lcd.print(humidity);
   lcd.print("%");
-  
+
   lcd.setCursor(0, 3);
   lcd.print("Pwred By ElectroYeti");
 
